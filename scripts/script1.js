d1b78c5c14cadaca10496e9fc4f4ea7f849f684f
Sergey Zavisha <sergey.zavisha@gmail.com>
2020-05-14 16:54:03 +0300

    Это мой первый комит

---
+++ scripts/script1.js
@@ -0,0 +1,12 @@
+'use strict';
+/*var number=5;
+var string = "Hello!";
+var sym = '2';
+var boolean = true;
+const Person = {name:"Sergey", height:180, weight:82};
+console.log(4/0);
+console.log(Person.height);*/
+//console.log(typeof(String(null)));
+//console.log(5 + +'5');
+//console.log(parseInt('15px',10));
+console.log("2"===2);
